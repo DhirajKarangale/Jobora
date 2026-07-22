a8420f5984eb0e36fce938ba69706da47f04b8fc
DhirajKarangale <dakarangale02@gmail.com>
2026-07-22 13:31:12 +0530

    feat: initialize main entry point for job workflow execution

--- ai/src/main.py
+++ ai/src/main.py
@@ -3,6 +3,9 @@ from workflow import manage_job_workflow
 
 def main():
     job_id_phonepe_backend = "77e0fda6-baaa-4963-adec-46a12755c63e"
+    job_id_eton_csharp = "04cfca07-f5c0-49c6-8f41-f033646a94c4"
+    job_id_hackjob_java = "4c847b0a-5e98-4e14-bf80-d4e507afa844"
+    job_id_eqnix_generic = "4c847b0a-5e98-4e14-bf80-d4e507afa844"
     manage_job_workflow(job_id_phonepe_backend)
 
 
