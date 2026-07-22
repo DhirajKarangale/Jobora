f35fd74441977a74c21193afd87abe473759ac72
DhirajKarangale <dakarangale02@gmail.com>
2026-07-22 18:26:30 +0530

    creating frontend project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import { connectRedis } from "./redis/index.ts";
 import { connectDb } from "./db/index.ts";
-import { edge, closeBrowser } from "./browserManager.ts";
+import { edge, closeBrowser } from "./utils/browserManager.ts";
 import linkedin from "./linkedin/index.ts";
 
 await connectDb();
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+
+function App() {
+  return (
+    <>
+      <Button>Click me</Button>
+      <Badge variant="destructive">Destructive</Badge>
+      <h1 className="text-green-500">-DK-</h1>
+    </>
+  )
+}
+
+export default App
